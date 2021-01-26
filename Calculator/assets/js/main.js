ccb180934db02da7ac8add3d8d504fee9995591e
Meron Melese <se.meron/melese@gmail.com>
2021-01-26 17:03:12 +0300

    Opr functions created

---
+++ Calculator/assets/js/main.js
@@ -0,0 +1,28 @@
+var firstNumber;
+var secondNumber;
+
+function addNum(firstNumber,secondNumber){
+    var Sum= firstNumber + secondNumber;
+
+    console.log("Sum = " +Sum)
+}
+
+function subtractNum(firstNumber,secondNumber){
+    let Dif= firstNumber - secondNumber;
+
+    console.log("Difference = "+Dif)
+}
+
+function multiplyNum(firstNumber,secondNumber){
+    let Prd= firstNumber * secondNumber;
+
+    console.log("Product = " +Prd)
+}
+
+
+function divideNum(firstNumber,secondNumber){
+    let Quot= firstNumber / secondNumber;
+
+    console.log("Quotient = " +Quot.toFixed(2))
+}
+

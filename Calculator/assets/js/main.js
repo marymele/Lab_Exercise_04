9b168ce296499388138666b6cc756280ec9ca0e3
Meron Melese <se.meron/melese@gmail.com>
2021-01-27 21:35:04 +0300

    function change

--- Calculator/assets/js/main.js
+++ Calculator/assets/js/main.js
@@ -1,10 +1,40 @@
-var firstNumber;
-var secondNumber;
 
-function addNum(firstNumber,secondNumber){
-    var Sum= firstNumber + secondNumber;
 
-    console.log("Sum = " +Sum)
+(function(){
+    const opr=prompt("Choose an operator (+,-,*,/)");
+   
+    var firstNumber;
+    var secondNumber;
+
+
+    if(opr=="-"){
+        firstNumber=prompt("Enter the first number");
+        secondNumber=prompt("Enter the second number");
+        subtractNum(firstNumber,secondNumber);}
+    else if(opr=="+"){addNum();}
+    else if(opr=="/"){
+        firstNumber=prompt("Enter the first number");
+        secondNumber=prompt("Enter the second number");
+        divideNum(firstNumber, secondNumber);}
+    else if(opr=="*"){multiplyNum()};
+
+    
+}) ();
+
+function addNum(){
+    var Sum=0;
+    var value= new Array();
+    var nums=prompt("Enter the number of values to be added")
+    for(var i=0;i<nums;i++){
+        x=prompt("Enter a value");
+        value[i]=x;
+    }
+
+   for(var i=0;i<nums;i++){
+       Sum+=parseInt(value[i]);
+   }
+   
+   console.log("Sum = " +Sum)
 }
 
 function subtractNum(firstNumber,secondNumber){
@@ -13,26 +43,43 @@ function subtractNum(firstNumber,secondNumber){
     console.log("Difference = "+Dif)
 }
 
-function multiplyNum(firstNumber,secondNumber){
-    let Prd= firstNumber * secondNumber;
+function multiplyNum(){
+    var Prd=1;
+    var value= new Array();
+    var nums=prompt("Enter the number of values to be multiplied ")
+    for(var i=0;i<nums;i++){
+        x=prompt("Enter a value");
+        value[i]=x;
+    }
+
+   for(var i=0;i<nums;i++){
+       Prd*=parseInt(value[i]);
+   }
 
     console.log("Product = " +Prd)
 }
 
 
 function divideNum(firstNumber,secondNumber){
-    let Quot= firstNumber / secondNumber;
+    let Quot= parseFloat(firstNumber) /parseFloat(secondNumber);
 
-    console.log("Quotient = " +Quot.toFixed(2))
+    console.log("Quotient = " +Quot)
 }
 
 
-(function(){
-    let opr=prompt("Choose an operator (+,-,*,/)");
-    if(opr=="-"){subtractNum(firstNumber,secondNumber);}
-    else if(opr=="/"){divideNum(firstNumber, secondNumber)}
-    firstNumber=prompt("Enter the first number");
-    secondNumber=prompt("Enter the second number")
 
-    
-}) ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
